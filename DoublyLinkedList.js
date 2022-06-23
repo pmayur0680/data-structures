94defeb7425d1aeb1fc614369ca0149d83078670
Mayur Patel <pmayur0680@gmail.com>
2022-06-23 07:46:08 -0500

    push for doubly linked list

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -11,4 +11,25 @@ class DoublyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+    // adding a node to the end of list
+    // create a new node with value passed in
+    // if not head, set head and tail to newnode
+    // else set tail next = newnode
+    // set previous on new node to old tail
+    //  tail = newnode
+    // increment length, return this
+    push(val) {
+        var newNode = new Node(val);
+        if(!this.head) {
+            // empty list
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
